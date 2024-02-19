f6f607e0c5e10dd0b6b1f0be4f77a6bb148e7a59
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-02-19 20:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function fibonacci(num) {
-	if(num==1){
-		console.log(0);
-	}else if(num==2){
-		console.log(0,1);
-	}else if(num>2){
-		console.log(0,1);
+let n1=0,n2=1,n3;
+function fibonacci(n) {
+	for(let i=0;i<n;i++){
+		console.log(n1);
+		n1=n2;
+		n3=n1+n2;
+		n2=n3;
 	}
 }
-
+fibonacci(prompt(n))
 module.exports = fibonacci;
